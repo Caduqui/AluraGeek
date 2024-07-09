2245eee098520fb8000b3f9c0ef56b858b37cef3
Caduqui <guilhermelimaconte2500@gmail.com>
2024-07-09 10:42:31 -0400

    retornando preventDefault

--- js/criarCard.js
+++ js/criarCard.js
@@ -2,7 +2,8 @@ import { conectaApi } from "./conectaApi.js";
 
 const formulario = document.querySelector('[data-formulario]');
 
-async function criarCard() {
+async function criarCard(evento) {
+    evento.preventDefault();
     const nome = document.querySelector('[data-nome]').value;
     const preco = document.querySelector('[data-preco]').value;
     const imagem = document.querySelector('[data-imagem]').value;
